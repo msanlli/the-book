b47abdc9a1283ab9b1bf20a68dfce9bab6578300
msanlli <marc@sanchis.dev>
2025-03-27 16:34:38 +0100

    guessing-game/initial commit

---
+++ guessing-game/target/debug/deps/guessing_game-6d3888879084f95b.d
@@ -0,0 +1,5 @@
+/Users/nomad/nomad_archive/studies/rust/the-book/guessing-game/target/debug/deps/libguessing_game-6d3888879084f95b.rmeta: src/main.rs
+
+/Users/nomad/nomad_archive/studies/rust/the-book/guessing-game/target/debug/deps/guessing_game-6d3888879084f95b.d: src/main.rs
+
+src/main.rs:
---
+++ guessing-game/target/debug/deps/guessing_game-8859ccf5d60c788a.d
@@ -0,0 +1,5 @@
+/Users/nomad/nomad_archive/studies/rust/the-book/guessing-game/target/debug/deps/libguessing_game-8859ccf5d60c788a.rmeta: src/main.rs
+
+/Users/nomad/nomad_archive/studies/rust/the-book/guessing-game/target/debug/deps/guessing_game-8859ccf5d60c788a.d: src/main.rs
+
+src/main.rs:
